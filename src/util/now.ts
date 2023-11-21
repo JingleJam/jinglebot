ab61cf32fe1a7a2744f3d5fef03ced6cab2df053
MattIPv4 <me@mattcowley.co.uk>
2023-11-21 03:11:17 +0000

    Remove getNow override for testing

--- src/util/now.ts
+++ src/util/now.ts
@@ -1,5 +1,4 @@
 // Util to allow for easy overriding when testing
-// TODO: switch back to new Date() when we're done testing
-const getNow = () => new Date("2023-12-01T23:15:00.000Z");
+const getNow = () => new Date();
 
 export default getNow;
