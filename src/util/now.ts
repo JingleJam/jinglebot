c240f3592124381b3b398698fe067acf4735c793
MattIPv4 <me@mattcowley.co.uk>
2025-11-14 17:01:48 +0000

    Update date for staging

--- src/util/now.ts
+++ src/util/now.ts
@@ -1,4 +1,9 @@
 // Util to allow for easy overriding when testing
-const getNow = () => new Date();
+const getNow = () => {
+    const date = new Date();
+    date.setFullYear(2024);
+    date.setMonth(11);
+    return date;
+};
 
 export default getNow;
